dcad5c964638414ab6c678e8b019cc07f4724cb2
Vasanth Pappu <vasanth.pappu@gmail.com>
2014-11-16 19:25:36 -0500

    added Jasmine tests, first passing test

---
+++ spec/greenhouseSpec.js
@@ -0,0 +1,64 @@
+describe("Greenhouse", function() {
+  var greenhouse;
+
+  beforeEach(function() {
+    h = 400;
+    w = 400;
+    selector = '#visualization';
+    greenhouse = new Greenhouse(selector, w, h);
+  });
+
+  it("#new", function() {
+    expect(greenhouse.w).toEqual(w);
+    expect(greenhouse.h).toEqual(h);
+  });
+
+  xit("should be able to play a Song", function() {
+    player.play(song);
+    expect(player.currentlyPlayingSong).toEqual(song);
+
+    //demonstrates use of custom matcher
+    expect(player).toBePlaying(song);
+  });
+
+  describe("when song has been paused", function() {
+    beforeEach(function() {
+      player.play(song);
+      player.pause();
+    });
+
+    xit("should indicate that the song is currently paused", function() {
+      expect(player.isPlaying).toBeFalsy();
+
+      // demonstrates use of 'not' with a custom matcher
+      expect(player).not.toBePlaying(song);
+    });
+
+    xit("should be possible to resume", function() {
+      player.resume();
+      expect(player.isPlaying).toBeTruthy();
+      expect(player.currentlyPlayingSong).toEqual(song);
+    });
+  });
+
+  // demonstrates use of spies to intercept and test method calls
+  xit("tells the current song if the user has made it a favorite", function() {
+    spyOn(song, 'persistFavoriteStatus');
+
+    player.play(song);
+    player.makeFavorite();
+
+    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+  });
+
+  //demonstrates use of expected exceptions
+  describe("#resume", function() {
+    xit("should throw an exception if song is already playing", function() {
+      player.play(song);
+
+      expect(function() {
+        player.resume();
+      }).toThrowError("song is already playing");
+    });
+  });
+});
